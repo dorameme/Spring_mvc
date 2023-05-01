84b114ccaef9b0e4d60641c06bd8a4cb2dbb018a
dorameme <doradorav@naver.com>
2023-05-01 15:26:52 +0900

    HTTP 요청 데이터 - GET 쿼리 파라미터

---
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,31 @@
+package hello.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
+/*
+* 1.파라미터 전송기능
+* get쿼리 파라미터
+* */
+@WebServlet(name="requestParamServlet",urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName-> System.out.println(paramName+"="+request.getParameter(paramName)));
+        //이름-> paramName value->request.getParameter(paramName)
+        System.out.println("[단일 파라미터조회]");
+        String username=request.getParameter("username");
+        String age= request.getParameter("age");
+        System.out.println("username = " + username + ", age = " + age);
+
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        String[] usernames=request.getParameterValues("username");
+        for(String name:usernames){
+            System.out.println("name:"+name);//이렇게 같은 변수명에 여러개를 한번에 받을수도 있다.
+        }
+    }//이름이 중복되게 하나의 파라미터에 여러개의 정보를 보내면 우선순위에서 먼저잡힌애를 출력
+}//getParameterValues()-> 복수 파라미터 getParameter()->단일파라미터
